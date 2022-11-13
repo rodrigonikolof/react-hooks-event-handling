f803d647305b9d82c05f934c414d8185b7ca6590
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-11-13 15:39:52 +1100

    Done with assignment

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
+  function tickle(e) {
+    console.log(e);
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
 
 export default Tickler;
